ebc88076eb42352cf2d996e4c9df0ca6000fb86d
yash01rai <yash.rai603@gmail.com>
2022-01-27 07:47:52 +0530

    Log added

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,6 +55,8 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
+
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
